91152337a90ac28fc42cbe12098730a0d20e038d
zizhen.huang <53505495@qq.com>
2021-02-27 18:09:44 +0800

    Issue:fix table name

--- proxy/nosql/table.go
+++ proxy/nosql/table.go
@@ -2,13 +2,13 @@ package nosql
 
 const (
 	/**
-	自己的收藏夹，主题，系列等，单位是词条
+	渠道
 	*/
-	TableChannel = "channels"
+	TableChannel = "third_channels"
 	/**
-	购买的别人的系列，主题等
+	合作伙伴
 	 */
-	TablePartner = "partners"
+	TablePartner = "third_partners"
 	/**
 	数量自动增加
 	*/
